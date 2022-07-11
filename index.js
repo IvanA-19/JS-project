d1c038bed4f491335d6b0f8b254a0ba8a4b26198
IvanA-18 <100191862+IvanA-18@users.noreply.github.com>
2022-07-11 14:08:51 +0300

    1

--- index.js
+++ index.js
@@ -1,59 +1,31 @@
 const telegram_api = require("node-telegram-bot-api")
 
-const token = "5360851862:AAFgW2G3gyCwna0C2-VamTvBMlN8rvbY2vU"
+const token = "5196554118:AAFmp2s9bujMiB33RV0edCLBU98MPij1R_o"
 
 const bot = new telegram_api(token, {polling: true})
 
-const chats = {}
-
-bot.on("message", async msg => {
-    const chat_id = msg.chat.id
-
-    if(msg.text === 'Закрыть'){
-        await bot.sendMessage(chat_id, 'Закрываю клавиатуру', {
-            reply_markup: {
-                remove_keyboard: true
-            }
-        })
-    }else if(msg.text === 'Ответить') {
-        await bot.sendMessage(chat_id, 'Отвечаю', {
-            reply_markup: {
-                force_reply: true
-            }
-        })
+const keyboard = {
+    reply_markup: {
+        keyboard: [
+            [{text: 'Меню игр'}],
+            ['Команды бота', 'Информация о боте'],
+            [{text: 'Поддержка'}]
+        ],
+        one_time_keyboard: true,
+        resize_keyboard: true
     }
-    else{
-        await bot.sendMessage(chat_id, "Клавиатура", {
-            reply_markup: {
-                keyboard: [
-                    [{
-                        text: 'Отправить местоположение',
-                        request_location: true
-                    }],
-                    ['Ответить', 'Закрыть'],
-                    [{
-                        text: 'Отправить контакт',
-                        request_contact: true
-                    }]
-                ]
-            }
-        })
-    }
-})
-bot.setMyCommands([
-    {command: "/start", description: "Приветствие"},
-    ]
-)
+}
 
-const start = () => {
+const bot_buttons = () => {
+
     bot.on("message", async msg => {
-        const text = msg.text;
-        const chat_id = msg.chat.id;
+        const chat_id = msg.chat.id
+
         if (msg.text === "/start") {
-            return bot.sendMessage(chat_id, `Рад приветствовать, ${msg.from.first_name} ${msg.from.last_name}!`)
+            await bot.sendMessage(chat_id, `Рад приветствовать, ${msg.from.first_name} ${msg.from.last_name}!`)
+            await bot.sendMessage(chat_id, "Открываю меню", keyboard)
         }
-        //return bot.sendMessage(chat_id, "Прости, но я не понимаю тебя")
     })
 }
 
-start()
+bot_buttons()
